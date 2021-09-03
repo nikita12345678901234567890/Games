6eb2a1d356df4eaa481801afbe9b1b280f87628d
Nikita Ulianov <person@mectruck.com>
2021-09-02 18:57:36 -0700

    Square must be a struct

--- Chess/SharedLibrary/Square.cs
+++ Chess/SharedLibrary/Square.cs
@@ -2,17 +2,13 @@
 {
 #pragma warning disable CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
 #pragma warning disable CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
-    public class Square
+    public struct Square
 #pragma warning restore CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
 #pragma warning restore CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        public int X;
+        public int Y;
 
-        public Square()
-        {
-
-        }
         public Square(int x, int y)
         {
             X = x;
@@ -20,7 +16,7 @@
         }
 
         public static bool operator ==(Square a, Square b)
-            => a?.X == b?.X && a?.Y == b?.Y;
+            => a.X == b.X && a.Y == b.Y;
 
         public static bool operator !=(Square a, Square b)
             => !(a == b);
